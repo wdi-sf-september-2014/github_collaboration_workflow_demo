142464ad623598bb063399b5ae3116987e437d5d
msyinmei <yin.codes@gmail.com>
2014-11-18 11:13:37 -0800

    Yin added exponential functions square and square root and proposes goddess status

--- math.rb
+++ math.rb
@@ -1,15 +1,23 @@
 def add x, y
-  x+y
+  x + y
 end
 
 def subtract x, y
-  x-y
+  x - y
 end
 
 def multiply x, y
-  x*y
+  x * y
 end
 
 def divide x, y
-  x/y
+  x / y
+end
+
+def square x, y
+  x ** 2
+end
+
+def square_root x, y
+  x ** (1/2)
 end
