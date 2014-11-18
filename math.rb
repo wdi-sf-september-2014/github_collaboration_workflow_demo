09bd9463bc0e73c6d81dbfbac39d02e7341897ce
Alex Notov <anotov@gmail.com>
2014-11-17 18:58:34 -0800

    Spacing is the key!

--- math.rb
+++ math.rb
@@ -1,15 +1,15 @@
-def add x,y
+def add x, y
   x+y
 end
 
-def subtract x,y
+def subtract x, y
   x-y
 end
 
-def multiply x,y
+def multiply x, y
   x*y
 end
 
-def divide x,y
+def divide x, y
   x/y
 end
